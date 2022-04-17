0a55c7cbb47cabb887fa45dc596532e2b4659251
fraydd <fredya.chavesb@uqvirtual.edu.co>
2022-04-17 10:31:56 -0500

    Registrar organisado y los iconos colocados correctamente

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,11 +25,16 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                <img src="assets/icon/home.svg" alt="Home" width="20" height="20"> 
-                <!-- <span class="material-icons-outlined iconos">home</span> -->
+                <!-- <img src="assets/icons/home.svg" alt="Home" width="30" height="30">  -->
+                    <!-- <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#fff">
+                        <path 
+                            d="M0 0h24v24H0V0z" fill="none"/>
+                        <path d="M12 5.69l5 4.5V18h-2v-6H9v6H7v-7.81l5-4.5M12 3L2 12h3v8h6v-6h2v6h6v-8h3L12 3z"/>
+                    </svg> -->
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1z"/></svg>
                 </a>
                 <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -112,9 +117,11 @@
     <section class="mb-4">
 
       <!-- Github -->
-      <a href="https://github.com/fraydd/reg1.git"
-        ><i class="fab fa-github"></i
-      ><img src="images\GitHub-Mark-Light-120px-plus.png" alt="aaa" width="20" height="20"></a>
+      <a href="https://github.com/fraydd/reg1.git">
+          <i class="fab fa-github"></i>
+          <svg fill="#fff" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">    <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"/></svg>
+<!--         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+ -->        </a>
     </section>
     <!-- Section: Social media -->
   </div>
--- resources/views/usuarios/create.blade.php
+++ resources/views/usuarios/create.blade.php
@@ -1,61 +1,65 @@
 @extends('layouts.app')
 @section('content')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>create</title>
-</head>
+
 <body>
-    <h1>este es el create</h1>
+
+<div class="container" style="height: 400px; border: 1px solid #000; background: #fff"> 
     <form action="{{ url('/usuario' )}}" method="post"enctype='multipart/form-data'>
         @csrf
-        <label for="Nombre">Primer nombre</label>
-        <br>
-            <input type="string" name="Nombre1" id="Nombre1" placeholder="nombre">
-        <br>
-
-        <label for="Nombre">Segundo nombre</label>
-        <br>
-            <input type="string" name="Nombre2" id="Nombre2">
-        <br>
-
-        <label for="Nombre">Primer apellido</label>
-        <br>
-            <input type="string" name="Apellido1" id="Apellido1">
-        <br>
-
-        <label for="Nombre">Segundo apellido</label>
-        <br>
-            <input type="string" name="Apellido2" id="Apellido2">
-        <br>
-
-        <label for="Nombre">Telefono</label>
-        <br>
-            <input type="string" name="Tel" id="Tel">
-        <br>
-        
-        <label for="Nombre">Dirección</label>
-        <br>
-            <input type="string" name="Direccion" id="Direccion">
-        <br>
-
-        <label for="Nombre">Edad</label>
-        <br>
-            <input type="int" name="Edad" id="Edad">
-        <br>
-
-        <label for="Nombre">Fotografia</label>
-        <br>
-        <input type="file" name="foto" id="foto">
-        <br>
-
-        <input type="submit" value="Registrar">
+        <div class="row" > 
 
 
-    </form>
-</body>
-</html>
+            <div class="col">
+                <label for="Nombre">Nombres</label>
+                <br>
+                    <input type="string" name="Nombre1" id="Nombre1" placeholder="Primer nombre">
+                <br>
+                <br>
+                    <input type="string" name="Nombre2" id="Nombre2" placeholder="Segundo nombre">
+                <br>
+                <label for="Nombre">Apellidos</label>
+                <br>
+                    <input type="string" name="Apellido1" id="Apellido1" placeholder="Primer Apellido">
+                <br>
+                <br>
+                    <input type="string" name="Apellido2" id="Apellido2" placeholder="Segundo Apellido">
+                <br>
+            </div>
+
+
+            <div class="col" >
+                <label for="Nombre">Telefono</label>
+                <br>
+                    <input type="string" name="Tel" id="Tel">
+                <br>
+                <label for="Nombre">Dirección</label>
+                <br>
+                    <input type="string" name="Direccion" id="Direccion">
+                <br>
+                <label for="Nombre">Edad</label>
+                <br>
+                    <input type="int" name="Edad" id="Edad">
+                <br>
+            </div>
+                
+            
+            <div class="col" >
+                <div class="container" style="height: 200px;border: 1px solid #000 ;">
+
+                <label for="Nombre">Fotografia</label>
+                <br>
+                <input type="file" name="foto" id="foto">
+                <br>
+
+                
+                </div>
+                <div class="container" style="border: 1px solid #000 ;">
+                    <input type="submit" value="Registrar">
+                    </div>
+            </div>
+
+
+        </div>
+    </form>  
+</div>
 @endsection
